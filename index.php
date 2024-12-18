70898db45d75d167d826cf9f381e21261299acc9
Brezhneva13 <165768374+Brezhneva13@users.noreply.github.com>
2024-12-18 18:08:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -177,18 +177,20 @@
             <footer class="py-4 bg-light mt-auto">
                 <div class="container-fluid px-4">
                     <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Ваша компания 2023</div>
+                        <div class="text-muted">Copyright &copy; Ваша Компания 2023</div>
                         <div>
-                            <a href="#">Политика конфиденциальности</a>
+                            <a href="#!">Политика конфиденциальности</a>
                             &middot;
-                            <a href="#">Условия использования</a>
+                            <a href="#!">Условия использования</a>
                         </div>
                     </div>
                 </div>
             </footer>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"></script>
     <script src="js/scripts.js"></script>
 </body>
 </html>
